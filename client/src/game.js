61f521a2679e551ad49f3bef8fb601c09890e1ec
Sven Pfaller <sven@inrain.org>
2025-10-10 17:10:13 +0200

    game for client

--- client/src/game.js
+++ client/src/game.js
@@ -1,1 +1,13 @@
+/** Game UI logic. */
+
+import { Content } from "#util";
+
 /** Game UI. */
+export class GameElement extends HTMLElement {
+  #content = Content.render("#game-template");
+
+  connectedCallback() {
+    this.replaceChildren(...this.#content.nodes);
+  }
+}
+customElements.define("textworld-game", GameElement);
