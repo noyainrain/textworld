f81a7025aaebe9dfab998b540a79845399960146
Sven Pfaller <sven@inrain.org>
2025-10-17 16:59:35 +0200

    ✨ Set up a game for unit tests

--- textworld/tests/test_game.py
+++ textworld/tests/test_game.py
@@ -4,9 +4,14 @@ import logging
 
 from unittest import IsolatedAsyncioTestCase
 
+from textworld.game import Game
+
 class TestCase(IsolatedAsyncioTestCase):
     @classmethod
     def setUpClass(cls) -> None:
         # TESTING
         #logging.disable()
         logging.basicConfig()
+
+    def setUp(self) -> None:
+        self.game = Game()
