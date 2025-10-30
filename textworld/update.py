acf5b00a9e2b7d0f18504558d1b4cc193838070e
Sven Pfaller <sven@inrain.org>
2025-10-31 00:25:33 +0100

    ✨ Create a device table

--- textworld/update.py
+++ textworld/update.py
@@ -8,3 +8,12 @@ def update(db: Connection[Row]) -> None:
     """Plumbing: Update the database *db*."""
     with db:
         db.execute('CREATE TABLE IF NOT EXISTS players (id PRIMARY KEY, create_time)')
+        db.execute(
+            """
+            CREATE TABLE IF NOT EXISTS devices (
+                id PRIMARY KEY,
+                create_time,
+                token,
+                player_id REFERENCES players
+            )
+            """)
