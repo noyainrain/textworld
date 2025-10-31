e59de7f2dd5788b016e47b9b4299992a5db5f3c4
Sven Pfaller <sven@inrain.org>
2025-10-31 22:09:53 +0100

    ✨ Test the CLI in its own context

--- textworld/tests/test_main.py
+++ textworld/tests/test_main.py
@@ -1,6 +1,7 @@
 # pylint: disable=missing-docstring
 
 from asyncio import create_task
+from contextvars import Context
 from tornado.httpclient import AsyncHTTPClient
 
 from textworld.util import cancel
@@ -10,7 +11,7 @@ from .test_game import TestCase
 
 class MainTest(TestCase):
     async def test(self) -> None:
-        task = create_task(main())
+        task = create_task(main(), context=Context())
         try:
             await AsyncHTTPClient().fetch('http://localhost:8080/')
         except OSError:
