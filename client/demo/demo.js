27b66e240100dd2e66588faf06a449b70a2ab8bd
Sven Pfaller <sven@inrain.org>
2025-12-05 19:26:52 +0100

    WIP cat stars

--- client/demo/demo.js
+++ client/demo/demo.js
@@ -1,26 +1,42 @@
 import p5 from "p5";
-import { Circle, Triangle, body, h, w } from "#sticky";
+import { Circle, Triangle, body, h, px, w } from "#sticky";
 
 new p5((p) => {
-  // Cat TODO
   const model = new Circle(
-    h(1 / 2), 1 / 2, body(1 / 2, 1 / 2), { stroke: "pink", fill: "black" },
-    // Ear
-    new Triangle(1 / 4, 1 / 4, body(1 / 2, 1 / 4), { start: 1, end: 3 }),
-    // Eye
+    1, 1, body(1 / 2, 1 / 2), {},
+
+    // Stars
+    ...[...Array(128)].map(() => {
+      const r = px(p.random(1, 4));
+      return new Circle(r, r, body(p.random(), p.random()), { fill: "white" });
+    }),
+
+    // Stars
+    // repeated(
+    //   100,
+    //   new Circle(random(px(1), px(4)), body(random(), random())),
+    // ),
+
+    // Cat TODO
     new Circle(
-      1 / 4, 1 / 4 * 2 / 3, body(1 / 4, 1 / 2), {},
-      new Circle(1 / 2, w(1 / 2), body(1 / 2, 1 / 2), { fill: "pink" }),
-    ),
-    new Circle(
-      1 / 4, 1 / 4 * 2 / 3, body(3 / 4, 1 / 2), {},
-      new Circle(1 / 2, w(1 / 2), body(1 / 2, 1 / 2), { fill: "pink" }),
-    ),
-    // Mouth
-    new Circle(
-      1 / 8, 1 / 8, body(1 / 2, 5 / 6), { fill: "pink" },
-      new Circle(1, 1, body(0, 0), { fill: "black", end: 1 / 2 }),
-      new Circle(1, 1, body(1, 0), { fill: "black", end: 1 / 2 }),
+      h(1 / 2), 1 / 2, body(1 / 2, 1 / 2), { stroke: "pink", fill: "black" },
+      // Ear
+      new Triangle(1 / 4, 1 / 4, body(1 / 2, 1 / 4), { start: 1, end: 3 }),
+      // Eye
+      new Circle(
+        1 / 4, 1 / 4 * 2 / 3, body(1 / 4, 1 / 2), {},
+        new Circle(1 / 2, w(1 / 2), body(1 / 2, 1 / 2), { fill: "pink" }),
+      ),
+      new Circle(
+        1 / 4, 1 / 4 * 2 / 3, body(3 / 4, 1 / 2), {},
+        new Circle(1 / 2, w(1 / 2), body(1 / 2, 1 / 2), { fill: "pink" }),
+      ),
+      // Mouth
+      new Circle(
+        1 / 8, 1 / 8, body(1 / 2, 5 / 6), { fill: "pink" },
+        new Circle(1, 1, body(0, 0), { fill: "black", end: 1 / 2 }),
+        new Circle(1, 1, body(1, 0), { fill: "black", end: 1 / 2 }),
+      ),
     ),
   );
 
