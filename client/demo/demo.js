aad1cf11f7e3f6131c2a372588cff51c2394383f
Sven Pfaller <sven@inrain.org>
2025-12-14 02:45:17 +0100

    WIP whole cat, raw animated, verbose

--- client/demo/demo.js
+++ client/demo/demo.js
@@ -1,45 +1,77 @@
 import p5 from "p5";
-import { Circle, Triangle, body, edge, h, px, w } from "#sticky";
+import { Circle, Triangle, body, edge, h, px } from "#sticky";
 
 new p5((p) => {
+  const verbose = "verbose" in p.getURLParams();
+
   const earW = 1 / 3;
   const earH = 3 / 4 * earW;
   const model = new Circle(
-    1, 1, body(1 / 2, 1 / 2), {},
+    1, 1, body(1 / 2, 1 / 2), { stroke: null, fill: null },
 
     // Stars
     ...[...Array(128)].map(() => {
       const r = px(p.random(1, 4));
       return new Circle(r, r, body(p.random(), p.random()), { fill: "white" });
     }),
-
-    // Stars
     // repeated(
-    //   100,
+    //   128,
     //   new Circle(random(px(1), px(4)), body(random(), random())),
     // ),
 
-    // Cat TODO
+    // Cat
     new Circle(
       h(1 / 2), 1 / 2, body(1 / 2, 1 / 2), { stroke: "pink", fill: "black" },
-      // Ear
-      new Triangle(earW, earH, edge(0, 10 / 16, h(earH / 2 - earH / 8)), { start: 1, end: 3 }),
-      new Triangle(earW, earH, edge(0, 14 / 16, h(earH / 2 - earH / 8)), { start: 1, end: 3 }),
-      // Eye
+      // Tail
       new Circle(
-        1 / 4, 1 / 4 * 2 / 3, body(1 / 4, 1 / 2), {},
-        new Circle(1 / 2, w(1 / 2), body(1 / 2, 1 / 2), { fill: "pink" }),
+        1 / 16, 1, body(1 / 2, 1 - 1 / 16 / 2),
+        { orientation: 12 / 16, stroke: "pink", start: 1 / 2 },
+        // new Circle(
+        //   1 / 3, 1 / 3, body(1 / 2, 1 / 3 / 16),
+        //   { orientation: 1 / 16, stroke: "pink", start: 1 / 2 },
+        // ),
       ),
+      // Cover
+      new Circle(1, 1),
+      // Face
       new Circle(
-        1 / 4, 1 / 4 * 2 / 3, body(3 / 4, 1 / 2), {},
-        new Circle(1 / 2, w(1 / 2), body(1 / 2, 1 / 2), { fill: "pink" }),
+        1 / 2, 1 / 2, body(1 / 2, 1 / 2 / 2), { stroke: null, fill: null },
+        // Eyes
+        new Circle(
+          1 / 4, 1 / 4, body(1 / 4, 1 / 2), { stroke: "pink", fill: "black" },
+          new Circle(1 / 2, 1 / 2, body(1 / 2, 1 / 2), { fill: "pink" }),
+        ),
+        new Circle(
+          1 / 4, 1 / 4, body(3 / 4, 1 / 2), { stroke: "pink", fill: "black" },
+          new Circle(1 / 2, 1 / 2, body(1 / 2, 1 / 2), { fill: "pink" }),
+        ),
+        // Sleepy eyes
+        new Circle(
+          1 / 4, 1 / 4, body(1 / 4, 1 / 2), { stroke: "pink", end: 1 / 2 },
+        ),
+        new Circle(
+          1 / 4, 1 / 4, body(3 / 4, 1 / 2), { stroke: "pink", end: 1 / 2 },
+        ),
+        // Mouth
+        new Circle(
+          1 / 8, 1 / 8, body(1 / 2, 5 / 6), {},
+          new Circle(1, 1, body(0, 0), { stroke: "pink", fill: "black", end: 1 / 2 }),
+          new Circle(1, 1, body(1, 0), { stroke: "pink", fill: "black", end: 1 / 2 }),
+          // new Triangle(
+          //   1 / 2, 1 / 2, body(1 / 2, -1 / 2 / 2), { orientation: 1 / 2, fill: "pink" }
+          // ),
+        ),
       ),
-      // Mouth
-      new Circle(
-        1 / 8, 1 / 8, body(1 / 2, 5 / 6), { fill: "pink" },
-        new Circle(1, 1, body(0, 0), { fill: "black", end: 1 / 2 }),
-        new Circle(1, 1, body(1, 0), { fill: "black", end: 1 / 2 }),
+      // Ears
+      new Triangle(
+        earW / 2, earH / 2, edge(0, 22 / 32, h(earH / 2 / 2 - earH / 2 / 8)), { start: 1, end: 3 },
       ),
+      new Triangle(
+        earW / 2, earH / 2, edge(0, 26 / 32, h(earH / 2 / 2 - earH / 2 / 8)), { start: 1, end: 3 },
+      ),
+      // Paws
+      new Circle(1 / 8, 1 / 8, body(1 / 2 - 1 / 8 / 2, 1 - 1 / 8 / 4), { end: 1 / 2 }),
+      new Circle(1 / 8, 1 / 8, body(1 / 2 + 1 / 8 / 2, 1 - 1 / 8 / 4), { end: 1 / 2 }),
     ),
   );
 
@@ -48,10 +80,75 @@ new p5((p) => {
   };
 
   p.draw = () => {
+    /**
+     * @param {number} progress
+     */
+    function easeIn(progress) {
+      return 1 - Math.cos(progress * Math.PI / 2);
+    }
+
+    /**
+     * @param {number} progress
+     */
+    // eslint-disable-next-line no-unused-vars
+    function easeOn(progress) {
+      return 1;
+    }
+
+    let duration = 2;
+    let pause = 1;
+    let offset = duration;
+    let t = (p.millis() / 1000 + offset) / (duration + pause) % 1;
+    t = t * (duration + pause) / duration;
+    if (t >= 1) {
+      t = 0;
+    } else {
+      t *= 2;
+      t = t >= 1 ? 2 - t : t;
+      t = easeIn(t);
+    }
+    let from = 12 / 16;
+    let to = 13 / 16;
+    const orientation = from + (to - from) * t;
+
+    // from = 1 / 16;
+    // to = -1 / 16;
+    // const orientation2 = from + (to - from) * t;
+
+    duration = 4;
+    pause = 12;
+    offset = duration;
+    t = (p.millis() / 1000 + offset) / (duration + pause) % 1;
+    t = t * (duration + pause) / duration;
+    if (t >= 1) {
+      t = 0;
+    } else {
+      t = easeOn(t);
+    }
+    const color = t ? null : "pink";
+
+    // @ts-ignore
+    model.links[128].links[0].orientation = orientation;
+    // model.links[128].links[0].links[0].orientation = orientation2;
+    // @ts-ignore
+    model.links[128].links[2].links[0].stroke = color;
+    // @ts-ignore
+    model.links[128].links[2].links[0].links[0].fill = color;
+    // @ts-ignore
+    model.links[128].links[2].links[1].stroke = color;
+    // @ts-ignore
+    model.links[128].links[2].links[1].links[0].fill = color;
+
     p.background("black");
-    p.noFill();
-    // const t = (p.millis() / 1000) / 4 % 1;
-    // model.links[128].links[0].at = edge(0, t, h(1 / 4 / 2));
     model.render(p);
+
+    if (verbose) {
+      p.fill("white");
+      p.textSize(16);
+      p.textAlign(p.RIGHT, p.TOP);
+      p.text(
+        `${p.frameRate().toFixed()} fps\n${p.width} x ${p.height} @ ${p.pixelDensity()}`,
+        p.width - p.textSize(), p.textSize());
+    }
   };
 });
